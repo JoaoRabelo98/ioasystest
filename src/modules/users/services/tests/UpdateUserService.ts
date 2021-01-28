19eea19f1aaa192b3ed346cc065244032dac090c
joao.rezende <joaorabello99@gmail.com>
2021-01-28 20:37:27 -0300

    test: test update user service when user is invalid

---
+++ src/modules/users/services/tests/UpdateUserService.ts
@@ -0,0 +1,29 @@
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
+import { uuid } from 'uuidv4';
+import CreateUserService from '../CreateUserService';
+import UpdateUserService from '../UpdateUserService';
+
+describe('Update User Service context', () => {
+  let userRepository: FakeUsersRepository;
+  let service: UpdateUserService;
+
+  beforeEach(() => {
+    userRepository = new FakeUsersRepository();
+    service = new UpdateUserService(userRepository);
+  });
+
+  it('should not be able to update when user is invalid', async () => {
+    expect.hasAssertions();
+
+    try {
+      await service.execute({
+        id: uuid(),
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(AppError);
+      expect(error.message).toBe('User not found');
+    }
+  });
+});
