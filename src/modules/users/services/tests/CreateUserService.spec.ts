ecd96c2ea364a2a5d45e988eddf49ace871a215c
joao.rezende <joaorabello99@gmail.com>
2021-01-28 17:40:58 -0300

    test: test when email is already used

--- src/modules/users/services/tests/CreateUserService.spec.ts
+++ src/modules/users/services/tests/CreateUserService.spec.ts
@@ -1,5 +1,6 @@
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
 import CreateUserService from '../CreateUserService';
 
 describe('Create User Service context', async () => {
@@ -26,5 +27,26 @@ describe('Create User Service context', async () => {
     expect(password).toBe(fakePassword);
     expect(name).toBe('John Doe');
     expect(email).toBe(fakeMail);
+  });
+
+  it('should not be able to create user when email is already used', async () => {
+    const fakePassword = `${Date.now()}`;
+    const fakeMail = `${Date.now()}@mail.com`;
+    await service.execute({
+      email: fakeMail,
+      name: 'John Doe',
+      password: fakePassword,
+    });
+
+    try {
+      await service.execute({
+        email: fakeMail,
+        name: 'Fake John Doe',
+        password: fakePassword,
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(AppError);
+      expect(error.message).toBe('Email address already used');
+    }
   });
 });
