acde326c949428e61a320e1b2289a8b9ee5ae9f8
joao.rezende <joaorabello99@gmail.com>
2021-01-28 19:08:03 -0300

    test: test authenticate user when password and email not match

--- src/modules/users/services/tests/AuthenticateUserService.spec.ts
+++ src/modules/users/services/tests/AuthenticateUserService.spec.ts
@@ -2,15 +2,18 @@ import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHa
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import AppError from '@shared/errors/AppError';
 import AuthenticateUserService from '../AuthenticateUserService';
+import CreateUserService from '../CreateUserService';
 
 describe('Authenticate User Service context', () => {
   let usersRepository: FakeUsersRepository;
   let hashProvider: FakeHashProvider;
   let service: AuthenticateUserService;
+  let createUserService: CreateUserService;
 
   beforeEach(() => {
     usersRepository = new FakeUsersRepository();
     hashProvider = new FakeHashProvider();
+    createUserService = new CreateUserService(usersRepository, hashProvider);
     service = new AuthenticateUserService(usersRepository, hashProvider);
   });
 
@@ -19,6 +22,23 @@ describe('Authenticate User Service context', () => {
 
     try {
       await service.execute({ email: 'fake-mail', password: 'fake-password' });
+    } catch (error) {
+      expect(error).toBeInstanceOf(AppError);
+      expect(error.message).toBe('Incorrect email/password combination');
+    }
+  });
+
+  it('should not be able to authenticate user when password and email not match', async () => {
+    expect.hasAssertions();
+
+    await createUserService.execute({
+      email: 'fake-email',
+      name: 'fake-name',
+      password: 'fake-password',
+    });
+
+    try {
+      await service.execute({ email: 'fake-email', password: 'invalid-password' });
     } catch (error) {
       expect(error).toBeInstanceOf(AppError);
       expect(error.message).toBe('Incorrect email/password combination');
