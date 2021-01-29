7603535570abaa23b4926c75ada1faaf8a91b44b
joao.rezende <joaorabello99@gmail.com>
2021-01-28 22:45:42 -0300

    feat: implements controller of update user

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import { classToClass } from 'class-transformer';
+import UpdateUserService from '@modules/users/services/UpdateUserService';
 
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -13,6 +14,21 @@ export default class UsersController {
     const userCreated = await createUserService.execute({
       email,
       password,
+      name,
+    });
+
+    return response.json(classToClass(userCreated));
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+    const { id } = request.user;
+
+    const updateUserService = container.resolve(UpdateUserService);
+
+    const userCreated = await updateUserService.execute({
+      id,
+      email,
       name,
     });
 
