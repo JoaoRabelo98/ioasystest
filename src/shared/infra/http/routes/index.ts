fec030ee5ba81d00a20a5227f58744e99e10c05f
joao.rezende <joaorabello99@gmail.com>
2021-01-30 13:50:55 -0300

    add actors on routing

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,11 +4,13 @@ import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersAdminRoutes from '@modules/users/infra/http/routes/usersAdmin.routes';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ensureAdmin from '@modules/users/infra/http/middlewares/ensureAdmin';
+import actorsRoute from '@modules/actors/infra/http/routes/actors.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/admin', ensureAuthenticated, ensureAdmin, usersAdminRoutes);
 routes.use('/sessions', sessionsRouter);
+routes.use('/actors', actorsRoute);
 
 export default routes;
