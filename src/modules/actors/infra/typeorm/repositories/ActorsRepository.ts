3948345e524af05e06ccbf7829a4188df2c953c3
joao.rezende <joaorabello99@gmail.com>
2021-01-30 13:35:04 -0300

    feat: implements repository of actors

---
+++ src/modules/actors/infra/typeorm/repositories/ActorsRepository.ts
@@ -0,0 +1,20 @@
+import ICreateActorDTO from '@modules/actors/dtos/ICreateActorDTO';
+import IActorsRepository from '@modules/actors/repositories/IActorsRepository';
+import { Repository } from 'typeorm';
+import Actor from '../entities/Actor';
+
+export default class ActorsRepository implements IActorsRepository {
+  private ormRepository: Repository<Actor>;
+
+  public async crate({ name }: ICreateActorDTO): Promise<Actor> {
+    const actorTocreate = this.ormRepository.create({
+      name,
+    });
+
+    return this.ormRepository.create(actorTocreate);
+  }
+
+  findById(id: string): Promise<Actor | undefined> {
+    return this.ormRepository.findOne(id);
+  }
+}
