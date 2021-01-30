87f36dacd3a5ae3479c11973063bca51cebd6728
joao.rezende <joaorabello99@gmail.com>
2021-01-30 19:19:13 -0300

    feat: implements create moovie service

---
+++ src/modules/moovies/dtos/ICreateMoovieWithActorDTO.ts
@@ -0,0 +1,6 @@
+export default interface ICrateMoovieWithActorDTO {
+  name: string;
+  director: string;
+  genre: string;
+  actors: Array<string>;
+}
---
+++ src/modules/moovies/services/CreateMoovieService.ts
@@ -0,0 +1,35 @@
+import IActorsRepository from '@modules/actors/repositories/IActorsRepository';
+import { inject, injectable } from 'tsyringe';
+import ICrateMoovieWithActorDTO from '../dtos/ICreateMoovieWithActorDTO';
+import Moovie from '../infra/typeorm/entities/Moovie';
+import IMooviesRepository from '../repositories/IMooviesRepository';
+
+@injectable()
+export default class CreateMoovieService {
+  constructor(
+    @inject('MooviesRepository')
+    private mooviesRepository: IMooviesRepository,
+
+    @inject('ActorsRepository')
+    private actorsRepository: IActorsRepository,
+  ) {}
+
+  public async execute({
+    name,
+    genre,
+    director,
+    actors,
+  }: ICrateMoovieWithActorDTO): Promise<Moovie> {
+    const moovie = await this.mooviesRepository.create({
+      name,
+      genre,
+      director,
+    });
+
+    const actorsFinded = await this.actorsRepository.findByIds(actors);
+
+    moovie.actors = actorsFinded;
+
+    return this.mooviesRepository.save(moovie);
+  }
+}
