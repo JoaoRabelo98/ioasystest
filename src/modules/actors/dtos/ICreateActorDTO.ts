350ba118cb295afd1cabc1d36a5b0de86d3419c0
joao.rezende <joaorabello99@gmail.com>
2021-01-30 13:31:20 -0300

    feat: create fake actor repository and actor dto

---
+++ src/modules/actors/dtos/ICreateActorDTO.ts
@@ -0,0 +1,3 @@
+export default interface ICreateActorDTO {
+  name: string;
+}
---
+++ src/modules/actors/repositories/fakes/FakeActorRepository.ts
@@ -0,0 +1,23 @@
+import ICreateActorDTO from '@modules/actors/dtos/ICreateActorDTO';
+import Actor from '@modules/actors/infra/typeorm/entities/Actor';
+import { uuid } from 'uuidv4';
+import IActorsRepository from '../IActorsRepository';
+
+export default class FakeActorRepository implements IActorsRepository {
+  private actors: Array<Actor> = [];
+
+  public async crate({ name }: ICreateActorDTO): Promise<Actor> {
+    const actor = {
+      id: uuid(),
+      name,
+    } as Actor;
+
+    this.actors.push(actor);
+
+    return actor;
+  }
+
+  public async findById(id: string): Promise<Actor | undefined> {
+    return this.actors.find(actor => actor.id === id);
+  }
+}
