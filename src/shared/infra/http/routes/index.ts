f9648c6e036ba5b219c6d1cf8dfe2e558353d3a3
joao.rezende <joaorabello99@gmail.com>
2021-01-29 23:21:34 -0300

    feat: implements middleware on admin route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,12 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersAdminRoutes from '@modules/users/infra/http/routes/usersAdmin.routes';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ensureAdmin from '@modules/users/infra/http/middlewares/ensureAdmin';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
-routes.use('/admin', ensureAuthenticated, usersAdminRoutes);
+routes.use('/admin', ensureAuthenticated, ensureAdmin, usersAdminRoutes);
 routes.use('/sessions', sessionsRouter);
 
 export default routes;
