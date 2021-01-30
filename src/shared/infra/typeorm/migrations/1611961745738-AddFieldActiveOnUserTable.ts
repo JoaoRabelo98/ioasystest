d72f80e7a9996d709e2ec20dfd3fe5ead133a82a
joao.rezende <joaorabello99@gmail.com>
2021-01-29 22:06:47 -0300

    refactor: refactor eslint

--- src/shared/infra/typeorm/migrations/1611961745738-AddFieldActiveOnUserTable.ts
+++ src/shared/infra/typeorm/migrations/1611961745738-AddFieldActiveOnUserTable.ts
@@ -1,14 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddFieldActiveOnUserTable1611961745738 implements MigrationInterface {
-    name = 'AddFieldActiveOnUserTable1611961745738'
+export default class AddFieldActiveOnUserTable1611961745738
+  implements MigrationInterface {
+  name = 'AddFieldActiveOnUserTable1611961745738';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "active" boolean NOT NULL DEFAULT true`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "active" boolean NOT NULL DEFAULT true`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "active"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "active"`);
+  }
 }
