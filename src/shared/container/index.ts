f32d9cd160d8e3ce89a83c8b81fe4f1ce71d181d
joao.rezende <joaorabello99@gmail.com>
2021-01-30 17:48:01 -0300

    feat: implements moovies on dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,12 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IActorsRepository from '@modules/actors/repositories/IActorsRepository';
 import ActorsRepository from '@modules/actors/infra/typeorm/repositories/ActorsRepository';
+import IMooviesRepository from '@modules/moovies/repositories/IMooviesRepository';
+import MooviesRepository from '@modules/moovies/infra/typeorm/repositories/MooviesRepository';
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UserRepository);
 container.registerSingleton<IActorsRepository>('ActorsRepository', ActorsRepository);
+container.registerSingleton<IMooviesRepository>(
+  'MooviesRepository',
+  MooviesRepository,
+);
