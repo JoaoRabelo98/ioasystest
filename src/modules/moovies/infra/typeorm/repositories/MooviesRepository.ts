ab7e25f55db8e31b6421bdc2deeebed2646e8a5d
joao.rezende <joaorabello99@gmail.com>
2021-01-30 17:25:50 -0300

    feat: implements repository of moovie

---
+++ src/modules/moovies/infra/typeorm/repositories/MooviesRepository.ts
@@ -0,0 +1,32 @@
+import ICreateMoovieDTO from '@modules/moovies/dtos/ICreateMoovieDTO';
+import IMooviesRepository from '@modules/moovies/repositories/IMooviesRepository';
+import { getRepository, Repository } from 'typeorm';
+import Moovie from '../entities/Moovie';
+
+export default class MooviesRepository implements IMooviesRepository {
+  private ormRepository: Repository<Moovie>;
+
+  constructor() {
+    this.ormRepository = getRepository(Moovie);
+  }
+
+  public async create(moovieData: ICreateMoovieDTO): Promise<Moovie> {
+    const moovie = this.ormRepository.create(moovieData);
+
+    await this.ormRepository.save(moovie);
+
+    return moovie;
+  }
+
+  public async save(moovie: Moovie): Promise<Moovie> {
+    return this.ormRepository.save(moovie);
+  }
+
+  public async findOne(id: string): Promise<Moovie | undefined> {
+    return this.ormRepository.findOne(id);
+  }
+
+  findAll(): Promise<Moovie[]> {
+    return this.ormRepository.find();
+  }
+}
