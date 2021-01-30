07836fc2adf7b592337bbf1b7d10ff3e6601c629
joao.rezende <joaorabello99@gmail.com>
2021-01-30 13:36:55 -0300

    feat: add actors repository on dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,5 +4,8 @@ import '@modules/users/providers';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import IActorsRepository from '@modules/actors/repositories/IActorsRepository';
+import ActorsRepository from '@modules/actors/infra/typeorm/repositories/ActorsRepository';
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UserRepository);
+container.registerSingleton<IActorsRepository>('ActorsRepository', ActorsRepository);
