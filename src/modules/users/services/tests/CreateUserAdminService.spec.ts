a0ee07f2d6846075f69111914cae7e05bdf04a64
joao.rezende <joaorabello99@gmail.com>
2021-01-29 22:27:05 -0300

    test: test create admin when email is already using

--- src/modules/users/services/tests/CreateUserAdminService.spec.ts
+++ src/modules/users/services/tests/CreateUserAdminService.spec.ts
@@ -1,5 +1,6 @@
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
 import { hash } from 'bcryptjs';
 import CreateUserAdminService from '../CreateUserAdminService';
 
@@ -23,5 +24,25 @@ describe('Create User Admin Service context', () => {
 
     expect(createdAdmin.id).not.toBeUndefined();
     expect(createdAdmin.isAdmin).toBe(true);
+  });
+
+  it('should not be able to create a admin when email is already using', async () => {
+    expect.hasAssertions();
+
+    await service.execute({
+      email: 'fake-mail@mail.com',
+      name: 'fake-name',
+      password: 'fake-password',
+    });
+    try {
+      await service.execute({
+        email: 'fake-mail@mail.com',
+        name: 'fake-name',
+        password: 'fake-password',
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(AppError);
+      expect(error.message).toBe('Email address already used');
+    }
   });
 });
